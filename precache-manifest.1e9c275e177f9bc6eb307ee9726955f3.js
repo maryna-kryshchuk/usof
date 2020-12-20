012efbd6da88ed449074c1089492621639f0525f
ystasivs <no1emailname@gmail.com>
2020-12-20 18:33:40 +0200

    Updates

---
+++ precache-manifest.1e9c275e177f9bc6eb307ee9726955f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4304170f2fdcdcf723711d9a51245d19",
+    "url": "/usof/index.html"
+  },
+  {
+    "revision": "85bb68af010778534654",
+    "url": "/usof/static/css/main.723cd43c.chunk.css"
+  },
+  {
+    "revision": "516970b4e8e71d3362a6",
+    "url": "/usof/static/js/2.cf5e0c2f.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/usof/static/js/2.cf5e0c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85bb68af010778534654",
+    "url": "/usof/static/js/main.2ae65bbf.chunk.js"
+  },
+  {
+    "revision": "0d588a82f861d39e42bc",
+    "url": "/usof/static/js/runtime-main.1bcfb7fe.js"
+  }
+]);
